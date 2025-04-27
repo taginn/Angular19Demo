9d2e7b1dd42e32164979e3364c6e32df14d139c4
Tom Ginn <tom.a.ginn@gmail.com>
2025-04-27 19:46:16 -0400

    Feature/add-ngrx (#2)
    * feat: add NgRx dependencies and VSCode settings for workspace
    
    * feat: add NgRx store and effects providers to app configuration
    
    * feat: update NgRx dependencies to version 19.1.0

--- my-workspace/apps/my-angular-app/src/app/app.config.ts
+++ my-workspace/apps/my-angular-app/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore(),
+    provideEffects(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
